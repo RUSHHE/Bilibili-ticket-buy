21cea112c304eac10c88312737fa7fd619899b2e
xmbhjQAQ <72352414+xmbhjQAQ@users.noreply.github.com>
2023-04-27 22:24:54 +0800

    CPP抢票支持！操作更简洁

--- cpp.py
+++ cpp.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from utils import EmailHelper
+import sys
 import json
 import random
 import time
@@ -14,7 +15,7 @@ if len(config["ccp_cookies"]) == 0:
     print("cookies未设置, 是否进行cookies获取?(手动登录后回到终端按任意键,程序将自动获取cookies)")
     getcookies = input("输入yes开始获取cookies:")
     if getcookies == "yes":
-        WebDriver = webdriver.Chrome()
+        WebDriver = webdriver.Edge()
         WebDriver.get("https://cp.allcpp.cn/#/ticket/detail?event=1074")
         print('=============================================')
         input("登录完成后请按任意键继续\n")
@@ -27,7 +28,26 @@ if len(config["ccp_cookies"]) == 0:
         print("未输入 yes, 程序结束")
         exit(1)
 
-WebDriver = webdriver.Chrome()
+#选票
+Switch = input("输入你想购买的票，数字1,2分别代表普通D1,D2;数字3,4分别代表VIP D1,D2；数字5为普通D1+D2套票")
+
+if Switch in ['1', '2', '3', '4', '5']:
+    if Switch == '1':
+       sum = "DAY1 普通票"
+    elif Switch == '2':
+       sum = "DAY2 普通票"
+    elif Switch == '3':
+       sum = "DAY1 VIP票"
+    elif Switch == '4':   
+       sum = "DAY2 VIP票"
+else:    
+     print("输入非法!请重新运行程序")
+     sys.exit()
+
+print("你选择了：",sum)
+
+
+WebDriver = webdriver.Edge()
 WebDriver.get("https://cp.allcpp.cn/#/ticket/detail?event=1074")
 print("进入购票页面成功")
 for cookie in config["ccp_cookies"]:
@@ -48,8 +68,26 @@ while True:
     currurl = WebDriver.current_url
     if "cp.allcpp.cn/#/ticket/detail" in currurl:
         try:
-            ticket = WebDriver.find_element(
-                By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY1 普通票']")  # 最后一项[]对应票的类型
+            if Switch == '1':
+                ticket = WebDriver.find_element(
+                    By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY1 普通票']")  # 最后一项[]对应票的类型
+            
+            elif Switch == '2':
+                ticket = WebDriver.find_element(
+                    By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY2 普通票']")  # 最后一项[]对应票的类型
+            
+            elif Switch == '3':
+                ticket = WebDriver.find_element(
+                    By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY1 VIP票']")  # 最后一项[]对应票的类型
+            
+            elif Switch == '4':
+                ticket = WebDriver.find_element(
+                    By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY2 VIP票']")  # 最后一项[]对应票的类型
+            
+            elif Switch == '5':
+                ticket = WebDriver.find_element(
+                    By.XPATH, "//*[@id='root']/div/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div[text()='DAY1+DAY2 普通票']")  # 最后一项[]对应票的类型                    
+            
             ticket.click()
             if ticket.get_attribute('class') == 'ticket-box disabled':
                 print("无票")
